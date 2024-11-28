3b6c57b630d353c412d971ede89eb3b8e96eb6f0
HansBug <hansbug@buaa.edu.cn>
2024-11-28 22:17:52 +0800

    dev(hansbug): try fix on windows

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 import logging
 import os
+import platform
 import re
 import subprocess
 import sys
 from codecs import open
+from distutils.version import LooseVersion
 
 from setuptools import Extension
 from setuptools import find_packages, setup
@@ -43,10 +45,15 @@ class CMakeExtension(Extension):
 class CMakeBuild(build_ext):
     def run(self):
         try:
-            subprocess.check_output(['cmake', '--version'])
+            out = subprocess.check_output(['cmake', '--version'])
         except OSError:
             raise RuntimeError("CMake must be installed to build the following extensions: " +
                                ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
         for ext in self.extensions:
             self.build_extension(ext)
@@ -61,14 +68,21 @@ class CMakeBuild(build_ext):
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
-        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-        build_args += ['--', '-j2']
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2 ** 32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
 
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
                                                               self.distribution.get_version())
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp, exist_ok=True)
+
         b1_cmds = ['cmake', ext.sourcedir] + cmake_args
         logging.info(f'Build with {b1_cmds!r} ...')
         subprocess.check_call(b1_cmds, cwd=self.build_temp, env=env)
